b255c540413a188ecfefbe27071fe91d694e042a
vuducnam2005 <vuducnam2005@users.noreply.github.com>
2026-06-08 02:48:29 +0000

    ....

--- src/types/medicine.ts
+++ src/types/medicine.ts
@@ -1,6 +1,9 @@
 export interface Medicine {
   medicineId: number
   medicineName: string
+  medicineType?: string
+  activeIngredient?: string
+  unit?: string
   dosageForm: string // e.g., 'Viên nén', 'Siro', 'Bột'
   unitPrice: number
   stockQuantity: number
